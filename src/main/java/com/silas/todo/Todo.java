2cbb8a206b759b355759d9f202c5ffe7e60b5e38
Silas Wu <silasawu@tamu.edu>
2022-06-21 20:01:42 -0500

    dummy function

---
+++ src/main/java/com/silas/todo/Todo.java
@@ -0,0 +1,65 @@
+package com.silas.todo;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Todo {
+    private int id;
+    private String title;
+    private String description;
+    private boolean completed;
+    private Date deadline;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return id == todo.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/com/silas/todo/TodoController.java
@@ -0,0 +1,33 @@
+package com.silas.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class TodoController {
+    @GetMapping("/todolist")
+    public List<Todo> all(){
+        List<Todo> todoList = new ArrayList<>();
+        Todo todo1 = new Todo();
+        todo1.setId(1);
+        todo1.setCompleted(false);
+        todo1.setDeadline(new Date());
+        todo1.setDescription("description");
+        todo1.setTitle("todo1");
+        todoList.add(todo1);
+
+        Todo todo2 = new Todo();
+        todo2.setId(2);
+        todo2.setCompleted(false);
+        todo2.setDeadline(new Date());
+        todo2.setDescription("description2");
+        todo2.setTitle("todo2");
+        todoList.add(todo2);
+
+        return todoList;
+    }
+}
